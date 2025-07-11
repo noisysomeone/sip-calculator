6a3cc4553195c2e035a210cda5ffa35855f44d9b
noisysomeone <noisysomeone@gmail.com>
2025-07-11 18:42:58 +0200

    grok new

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,12 @@
-import { defineConfig } from 'astro/config';
-import preact from '@astrojs/preact';
+import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  integrations: [preact()],
+  output: "static",
+  vite: {
+    build: {
+      rollupOptions: {
+        external: ["chart.js"],
+      },
+    },
+  },
 });
